b1152b04a0db29ba75bd138f81df2787ae4c7fdb
24ERIC <60751655+24ERIC@users.noreply.github.com>
2024-05-09 09:57:44 +0800

    3 try

--- api/hello.ts
+++
@@ -1,6 +0,0 @@
-// api/hello.ts
-import { VercelRequest, VercelResponse } from '@vercel/node';
-
-export default function(request: VercelRequest, response: VercelResponse): void {
-  response.status(200).send('hello');
-};
